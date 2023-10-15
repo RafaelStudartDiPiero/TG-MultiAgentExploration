f4914d4a2f796518c83a8f00a18c178630f3107d
ArthurJose2000 <arthur2000jose@gmail.com>
2023-10-15 11:54:41 -0300

    more results

--- my_results.py
+++ my_results.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import csv
 
-""" rows = []
-with open("tarry_1to40agents_250iterations_40x40.csv", "r") as f:
+rows = []
+with open("tarry_1to80agents_250iterations_20x20.csv", "r") as f:
     csvreader = csv.reader(f)
 
     for row in csvreader:
@@ -14,10 +14,10 @@ tarry_steps_row = rows[1]
 tarry_pionner_steps_row = rows[2]
 tarry_fraction_row = rows[3]
 tarry_stdev_row = rows[4]
-tarry_steps_from_first_to_last_row = rows[5] """
+tarry_steps_from_first_to_last_row = rows[5]
 
 rows = []
-with open("my_1to40agents_250iterations_30x30.csv", "r") as f:
+with open("my_1to80agents_250iterations_20x20.csv", "r") as f:
     csvreader = csv.reader(f)
 
     for row in csvreader:
@@ -29,7 +29,7 @@ my_fraction_row = rows[3]
 my_stdev_row = rows[4]
 my_fraction_pionner = rows[5]
 
-header = rows[0]
+""" header = rows[0]
 
 rows = []
 with open("my_1to40agents_250iterations_30x30_v2.csv", "r") as f:
@@ -42,22 +42,22 @@ my_v2_steps_row = rows[1]
 my_v2_pionner_steps_row = rows[2]
 my_v2_fraction_row = rows[3]
 my_v2_stdev_row = rows[4]
-my_v2_fraction_pionner = rows[5]
+my_v2_fraction_pionner = rows[5] """
 
-""" plt.plot(header, tarry_steps_row, label = "TARRY: Average of steps")
+plt.plot(header, tarry_steps_row, label = "TARRY: Average of steps")
 plt.plot(header, my_steps_row, label = "TG: Average of steps")
 
 plt.plot(header, tarry_pionner_steps_row, label = "TARRY: Pionner's average of steps")
-plt.plot(header, my_pionner_steps_row, label = "TG: Pionner's average of steps") """
+plt.plot(header, my_pionner_steps_row, label = "TG: Pionner's average of steps")
 
 """ plt.plot(header, tarry_fraction_row, label = "TARRY: Fraction of maze explored")
 #plt.plot(header, my_fraction_row, label = "TG: Fraction of maze explored")
 plt.plot(header, my_fraction_pionner, label = "TG: Fraction of maze explored until pionner") """
 
-plt.plot(header, my_steps_row, label = "TG - Average of steps")
+""" plt.plot(header, my_steps_row, label = "TG - Average of steps")
 plt.plot(header, my_pionner_steps_row, label = "TG - Pionner's average of steps")
 plt.plot(header, my_v2_steps_row, label = "TG (new) - Average of steps")
-plt.plot(header, my_v2_pionner_steps_row, label = "TG (new) - Pionner's average of steps")
+plt.plot(header, my_v2_pionner_steps_row, label = "TG (new) - Pionner's average of steps") """
 
 """ plt.plot(header, my_fraction_row, label = "TG - Fraction of maze explored")
 plt.plot(header, my_fraction_pionner, label = "TG - Fraction of maze explored until pionner")
@@ -74,7 +74,7 @@ plt.plot(header, tarry_steps_from_first_to_last_row, label = "TARRY: Average Ste
 plt.xlabel('No. of agents')
 plt.ylabel('No. of steps')
 #plt.ylabel('Fraction of maze explored')
-plt.title('TG vs TGv2 - 30-by-30 maze')
+plt.title('TG vs Tarry - 20-by-20 maze')
 
 plt.grid()
 plt.legend()
