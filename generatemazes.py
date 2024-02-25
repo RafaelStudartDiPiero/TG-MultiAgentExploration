aebba0e9389f1883b455e2e1b57e5e48df4ad770
RafaelStudartDiPiero <rafael.spiero@gmail.com>
2024-02-25 15:28:57 -0300

    Clean up for starting development

--- generatemazes.py
+++
@@ -1,9 +0,0 @@
-from pyamaze import maze
-
-for i in range(0, 250):
-    m=maze(100,100)
-    filename = 'mazes/100_by_100/maze_100x100__' + str(i + 1)
-    m.CreateMaze(loopPercent=0,theme='light', saveMaze=filename)
-
-    # Free up memory
-    m._win.destroy()
